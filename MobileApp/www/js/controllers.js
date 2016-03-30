0c56393944cc9c4c500cead2f192582027306796
Ravi Kiran <ravikirana32@gmail.com>
2016-03-30 22:10:25 +0530

    Mobile app basic api and ui added
    Mobile app basic api and ui added

--- MobileApp/www/js/controllers.js
+++ MobileApp/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout,ngGPlacesAPI,$rootScope) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -11,6 +11,100 @@ angular.module('starter.controllers', [])
 
   // Form data for the login modal
   $scope.loginData = {};
+  $rootScope.currentLatLong={};
+  $rootScope.currentLatLong.lat=0;
+  $rootScope.currentLatLong.lng=0;
+  $scope.hospitalName='';
+
+  $scope.getLocation=function() {
+    console.log("geolocation");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+};
+$scope.getLocation();
+function showPosition(position) {
+  console.log("showPosition");
+    console.log("Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude); 
+    $scope.lat=position.coords.latitude;
+    $scope.lon=position.coords.longitude;
+    $rootScope.currentLatLong.lat=position.coords.latitude;
+    $rootScope.currentLatLong.lng=position.coords.longitude;
+    $scope.search('hospital','hospital');//doctor,dentist,hospital
+}
+
+
+console.log("controller created");
+    $scope.user="ravi kiran";
+    $scope.loading=false;
+    $scope.key='';
+    //$scope.lat=12.9819373;
+    //$scope.lon= 77.73341219999999;
+    $scope.newdata=[];
+
+    
+    $scope.search=function(name,val){
+      console.log($scope.rangeInKm);
+        /*$ionicLoading.show({
+          content: 'Loading',
+          animation: 'fade-in',
+          showBackdrop: true,
+          maxWidth: 200,
+          showDelay: 0
+        });*/
+          var rad=5000;
+        $scope.slectedvalue=name;
+        $scope.datas = ngGPlacesAPI.nearbySearch({latitude:$scope.lat, longitude:$scope.lon,types:[val],radius:rad}).then(
+          function(data){
+            console.log("INSIDE                ");
+            if(data){
+              console.log("data");
+               console.log(data);
+              
+            }else{
+              console.log("EMPTY");
+            }
+           
+            
+            $scope.newdata=data;
+            //$scope.getDetails(data[0].reference);
+            //console.log($rootScope.newdata);
+            //$ionicLoading.hide();
+            return data;
+          }, function (response) {
+          console.log("ERROR");
+          console.log($scope.newdata);
+          $scope.newdata=[];
+        });
+        //$scope.$apply();
+        //console.log($scope.details);
+        //var newdata=JSON.parse($scope.datas.$$state);
+      //console.log(newdata);
+    };
+
+
+    $scope.getDetails=function(data){
+    /*$ionicLoading.show({
+      content: 'Loading',
+      animation: 'fade-in',
+      showBackdrop: true,
+      maxWidth: 200,
+      showDelay: 0
+    });*/
+    $scope.details = ngGPlacesAPI.placeDetails({reference:data}).then(
+    function (data) {
+      $scope.compltedata=data;
+      $rootScope.ravi=data;
+      console.log("complete data");
+      console.log(data)
+      //$ionicLoading.hide();
+      return data;
+    });
+  };
+  $scope.getDetails();
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -25,7 +119,8 @@ angular.module('starter.controllers', [])
   };
 
   // Open the login modal
-  $scope.login = function() {
+  $scope.login = function(name) {
+    $scope.hospitalName=name;
     $scope.modal.show();
   };
 
